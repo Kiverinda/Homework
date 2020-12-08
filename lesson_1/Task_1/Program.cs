fd376eb3f66c891093aadeee0f0683c851512c75
Dmitriy <kiverinda@yandex.ru>
2020-12-09 01:25:33 +0500

    Task_1

---
+++ lesson_1/Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now.ToLongDateString()}");
+            Console.Read();
+        }
+    }
+}
