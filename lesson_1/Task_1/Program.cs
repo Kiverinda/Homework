b7b5be671efb26e41bc9ac25f36a4d0a973af21a
Dmitriy <kiverinda@yandex.ru>
2020-12-09 00:58:58 +0500

    Homework 1

---
+++ lesson_1/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Today.ToLongDateString()}");
+            Console.Read();
+        }
+    }
+}
