97c558462ec02fe2545623f545fc868bfdb8f9d0
Dmitriy <kiverinda@yandex.ru>
2020-12-15 01:01:34 +0500

    added a solution to the problem using enum

--- lesson_2/Task_2/Program.cs
+++ lesson_2/Task_2/Program.cs
@@ -59,37 +59,49 @@ namespace Task_2
 
 
             // Вариант 2 (с проверкой введенных данных и использованием массива для хранения названия месяцев) 
-            string[] Month = {"Январь",
-                                "Февраль",
-                                "Март",
-                                "Апрель",
-                                "Май",
-                                "Июнь",
-                                "Июль",
-                                "Август",
-                                "Сентябрь",
-                                "Октябрь",
-                                "Ноябрь",
-                                "Декабрь" };
+            //string[] Month = {"Январь",
+            //                    "Февраль",
+            //                    "Март",
+            //                    "Апрель",
+            //                    "Май",
+            //                    "Июнь",
+            //                    "Июль",
+            //                    "Август",
+            //                    "Сентябрь",
+            //                    "Октябрь",
+            //                    "Ноябрь",
+            //                    "Декабрь" };
 
+            //Console.WriteLine("Введите номер месяца");
+            //do
+            //{
+            //    if (int.TryParse(Console.ReadLine(), out int numberMonth))
+            //    {
+            //        if (numberMonth > 0 && numberMonth < 13)
+            //        {
+            //            Console.WriteLine($"Месяц: {Month[numberMonth - 1]}");
+            //            break;
+            //        }
+            //        else Console.WriteLine("Не корректный ввод номера месяца");
+            //    }
+            //    else Console.WriteLine("Не корректный ввод номера месяца");
+            //} while (true);
+
+            // Вариант 3 (с использованием enum) 
             Console.WriteLine("Введите номер месяца");
-            do
-            {
-                if (int.TryParse(Console.ReadLine(), out int numberMonth))
-                {
-                    if (numberMonth > 0 && numberMonth < 13)
-                    {
-                        Console.WriteLine($"Месяц: {Month[numberMonth - 1]}");
-                        break;
-                    }
-                    else Console.WriteLine("Не корректный ввод номера месяца");
-                }
-                else Console.WriteLine("Не корректный ввод номера месяца");
-            } while (true);
-
-
-
+            int number = 0;
+            int.TryParse(Console.ReadLine(), out number);
+            if (((int)(Math.Pow(2, number - 1)) & (int)Months.Winter) == (int)(Math.Pow(2, number - 1))) Console.WriteLine("Зима");
             Console.ReadLine();
+        }
+
+        [Flags]
+        enum Months
+        {
+            January = 0b_000000000001,
+            February = 0b_000000000010,
+            December = 0b_100000000000,
+            Winter = January | February | December
         }
     }
 }
